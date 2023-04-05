218d39f1719b769b22baf01ad8b09e6585526d7a
GFrancoReduno~ <francoreduno@outlook.com>
2023-04-05 19:24:44 -0300

    tuplas

--- 05_listas.py
+++ 05_listas.py
@@ -23,4 +23,47 @@ print(my_list.count(7)) #cuenta la cantidad de veces que se repite el valor en u
 age, height, name = my_other_list #tiene que tener la misma cantidad de variables que datos dentro de la lista si o si, ni uno mas ni uno menos sino da error
 print(age)
 
- 
+print(my_list + my_other_list) #las listas solo se pueden sumar
+
+my_other_list.append("Florencia") #agrega un objeto al final de la lista
+print(my_other_list)
+
+my_other_list.insert(1, "Lola") #agrega un objeto en la posicion que le indiques
+print(my_other_list)
+
+my_other_list[1] = "Perro"
+print(my_other_list)
+
+my_other_list.remove("Perro") #remueve un objeto que le indiques
+print(my_other_list)
+
+my_list.remove(7) #si los objetos se repiten al eliminar elimina al primero solamente
+print(my_list)
+
+print(my_list.pop()) #saca el ultimo de la lista
+print(my_list)
+
+del my_list[2] #elimina un elemento por indice
+print(my_list)
+
+my_new_list = my_list.copy() #copia la lista en una nueva
+
+my_list.clear() #borra todos los elementos de la lista
+print(my_list)
+print(my_new_list)
+
+my_new_list.reverse() #da vuelta el orden de los indices en la lista
+print(my_new_list)
+
+my_new_list.sort() #ordena de menor a mayor
+print(my_new_list)
+
+print(my_new_list[1:3]) 
+
+my_list = "Florencia" #cambiar de lista a string
+print(my_list)
+print(type(my_list))
+
+
+
+
---
+++ 06_tuples.py
@@ -0,0 +1,22 @@
+#Tuples
+
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (23, 1.81, "Parka", "Parka")
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0])
+print(my_tuple[-1])
+#print(my_tuple[-5]) index error
+#print(my_tuple[5]) index error
+
+print(my_tuple.count("Parka"))
+print(my_tuple.index("Parka"))
+
+#my_tuple[1] = 1.83 NO SE PUEDE MODIFICAR LOS OBJETOS DENTRO DE LA TUPLA
+
+my_sum_tuple = my_tuple + my_other_tuple #se pueden sumar
+print(my_sum_tuple) 
+
