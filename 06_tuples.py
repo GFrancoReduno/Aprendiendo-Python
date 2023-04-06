212c6ba31501a8710bc49f41944bf56540f0a11e
GFrancoReduno~ <francoreduno@outlook.com>
2023-04-06 13:00:14 -0300

    final tuplas

--- 06_tuples.py
+++ 06_tuples.py
@@ -20,3 +20,20 @@ print(my_tuple.index("Parka"))
 my_sum_tuple = my_tuple + my_other_tuple #se pueden sumar
 print(my_sum_tuple)
 
+print(my_sum_tuple[3:6])
+
+my_tuple = list(my_tuple)  #se pueden realizar cambios en la tupla transformandola en lista
+print(type(my_tuple))
+
+my_tuple [2] = "Florencia"
+my_tuple.insert(3, "Lola")
+my_tuple = tuple(my_tuple)
+print(my_tuple)
+print(type(my_tuple)) #volvemos a transformar en tuple y se vuelve inmutable
+
+#del my_tuple[2] #TypeError: 'tuple' object doesn't support item deletion
+
+del my_tuple 
+#print(my_tuple) #NameError: name 'my_tuple' is not defined
+
+
